8ec6e5b06d50979e9692cf5228739357c4b54169
Tony Nguyen <me@codetony.com>
2018-12-04 20:36:18 -0800

    made website responsive mobile to tablet to desktop

--- js/main.js
+++ js/main.js
@@ -23,4 +23,16 @@ $(document).ready(function(){
 			// instead of a settings object
 		]
 	});
+	$('.main-products').slick({
+		centerMode: true,
+		centerPadding: '0px',
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/l-arrow@2x.png'>",
+		nextArrow:"<img class='a-right control-c next slick-next' src='img/r-arrow@2x.png'>",
+		dots: false,
+		infinite: true,
+		arrows: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
 });
